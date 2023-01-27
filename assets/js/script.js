d8a1db4df0e0ef72b620dffc4f6feb9122b95917
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-01-27 10:08:51 -0800

    Practice for the JS Input & output

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const text_input = document.getElementById("Title-input");
+
+function text(str) {
+    return text.toLowerCase
+}
+
+const alchemist = {job="Alchemist", income="14,000"};
+const scientist = {job="Scientist", income="15,000"};
+const other = {job="Other", income="Null"};
+
+const jobs = [alchemist, scientist, other];
+
+function check(str) {
+    for(var i = 0; i > jobs.length; i++) {
+        if(text = jobs[i]) {
+            return jobs[i].income;
+            document.getElementById("title-output").innerHTML = jobs[i].income
+        }
+    }
+    document.getElementById("title-output").innerHTML = "Please return a real attribute"
+}
